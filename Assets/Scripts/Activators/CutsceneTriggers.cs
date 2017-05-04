5dd2a707a329da20e39a91a446db7789e6d28366
recksbayne <recksbayne@gmail.com>
2017-05-04 01:59:59 -0400

    CUTSCENE AUDIO

--- Assets/Scripts/Activators/CutsceneTriggers.cs
+++ Assets/Scripts/Activators/CutsceneTriggers.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class CutsceneTriggers : MonoBehaviour {
 
 	public CutsceneController cutsceneController;
+	public AudioSource energyBallAudio;
 	// Use this for initialization
 	void Start () {
-		
+		energyBallAudio = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -22,9 +23,11 @@ public class CutsceneTriggers : MonoBehaviour {
 	public void CameraSwitchPos2() {
 		cutsceneController.cutsceneCam.transform.position = cutsceneController.cameraPos2.transform.position;
 		cutsceneController.cutsceneCam.transform.rotation = cutsceneController.cameraPos2.transform.rotation;
+
 	}
 
 	public void CameraSwitchPos3() {
 		cutsceneController.WhiteFadeInOut ();
+		energyBallAudio.Play ();
 	}
 }
