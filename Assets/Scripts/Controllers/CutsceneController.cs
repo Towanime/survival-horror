372eadd798fcc4610c9833ab8a39ce57a82fa5a6
recksbayne <recksbayne@gmail.com>
2017-05-03 22:00:58 -0400

    Cutscene progress

--- Assets/Scripts/Controllers/CutsceneController.cs
+++ Assets/Scripts/Controllers/CutsceneController.cs
@@ -6,7 +6,10 @@ using UnityEngine.UI;
 public class CutsceneController : MonoBehaviour {
 
 	public bool cutsceneActive;
-	public bool fadeActive;
+	public bool fadeInActiveBlack;
+	public bool fadeOutActiveBlack;
+	public bool fadeInActiveWhite;
+	public bool fadeOutActiveWhite;
 
 	public Color fadeColor;
 	public float fadeTime;
@@ -14,6 +17,8 @@ public class CutsceneController : MonoBehaviour {
 
 	public Image fadeImage;
 	public GameObject cutsceneCam;
+	public GameObject cameraPos1;
+	public GameObject cameraPos2;
 	public GameObject playerController;
 	public GameObject sunLotusParticles;
 	public GameObject pedestalParticles;
@@ -24,6 +29,7 @@ public class CutsceneController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		sunLotus.GetComponent<Animator> ().speed = 0;
 	}
 	
 	// Update is called once per frame
@@ -41,23 +47,60 @@ public class CutsceneController : MonoBehaviour {
 	}
 
 	void BlackFadeOut() {
-		if (fadeColor.a != 0 && cutsceneActive == true) {
-			fadeActive = true;
+		if (fadeColor.a != 0 && cutsceneActive == true && fadeInActiveBlack == false && fadeInActiveWhite == false && fadeOutActiveWhite == false) {
+			fadeOutActiveBlack = true;
 		}
 
-		if (fadeActive == true) {
+		if (fadeOutActiveBlack == true) {
 			fadeColor = fadeImage.color;
 			fadeColor.a = Mathf.Lerp(1, 0, currentTime/(fadeTime-currentTime));
 			currentTime += Time.deltaTime;
 			fadeImage.color = fadeColor;
 
 			if (fadeColor.a <= 0) {
-				fadeActive = false;
+				fadeOutActiveBlack = false;
 				LotusBloom ();
+				currentTime = 0;
 			}
 		}
 	}
+
+	public void WhiteFadeInOut() {
+
+		fadeImage.color = Color.white;
+
+		if (fadeColor.a == 0 && cutsceneActive == true) {
+			fadeInActiveWhite = true;
+		}
+
+		if (fadeInActiveWhite == true) {
+			fadeColor = fadeImage.color;
+			fadeColor.a = Mathf.Lerp (0, 1, currentTime / (fadeTime - currentTime));
+			currentTime += Time.deltaTime;
+			fadeImage.color = fadeColor;
+
+			if (fadeColor.a >= 100) {
+				fadeInActiveWhite = false;
+				fadeOutActiveWhite = true;
+				currentTime = 0;
+			}
+		}
+
+		if (fadeOutActiveWhite == true) {
+			fadeColor = fadeImage.color;
+			fadeColor.a = Mathf.Lerp (1, 0, currentTime / (fadeTime - currentTime));
+			currentTime += Time.deltaTime;
+			fadeImage.color = fadeColor;
+
+			if (fadeColor.a <= 0) {
+				fadeOutActiveWhite = false;
+				currentTime = 0;
+			}
+		}
+	}
+
 	void LotusBloom() {
+		sunLotus.GetComponent<Animator> ().speed = 1;
 		sunLotus.GetComponent<Animator> ().Play ("Unfold");
 	}
 }
